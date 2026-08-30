598b110ff3923f27102edfb2f235242e0fc4c65d
RyanDznwsk <ryan.gduzanowski@gmail.com>
2026-08-30 14:21:19 -0300

    Responsividade da Terceira Página Completa

--- PI - Produção de Alimentos para Grandes Populações/src/js/scriptNav.js
+++ PI - Produção de Alimentos para Grandes Populações/src/js/scriptNav.js
@@ -1,6 +1,8 @@
 const menuToggle = document.getElementById('menu-toggle');
 const navCell = document.getElementById('nav-cell');
+const lista = document.querySelector('.nav-cell ul');
 function openNavCell() {
+  lista.classList.add('display');
   navCell.classList.add('open');
   menuToggle.classList.add('open');
   document.body.style.overflow = 'hidden';
@@ -9,6 +11,11 @@ function closeNavCell() {
   navCell.classList.remove('open');
   menuToggle.classList.remove('open');
   document.body.style.overflow = '';
+  navCell.addEventListener('transitionend', () => {
+    if (!navCell.classList.contains('open')) {
+      lista.classList.remove('display');
+    }
+  }, { once: true });
 }
 menuToggle.addEventListener('click', (e) => {
   e.stopPropagation();
